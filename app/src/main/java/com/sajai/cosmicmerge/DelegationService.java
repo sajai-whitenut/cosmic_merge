b65f7ba602f16b156924077fbd2b242b38f9c1f9
sajai <sajai.whitenut@gmail.com>
2026-04-23 17:06:34 +0530

    fix manifest icon urls

---
+++ app/src/main/java/com/sajai/cosmicmerge/DelegationService.java
@@ -0,0 +1,14 @@
+package com.sajai.cosmicmerge;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
