542ebe31e01340ac5cef808593ab676806e34045
sajai <sajai.whitenut@gmail.com>
2026-04-23 21:36:58 +0530

    addad my game icons

---
+++ app/src/main/java/com/sajai/cosmicmerger/DelegationService.java
@@ -0,0 +1,14 @@
+package com.sajai.cosmicmerger;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
